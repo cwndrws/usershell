3cb3ffc6cf31ca5b182ea50815880c4e65738649
Charlie Andrews <charlieandrews.cwa@gmail.com>
2025-06-06 11:33:45 -0400

    Only export a function for making a user shell like this

--- flake.nix
+++ flake.nix
@@ -1,33 +1,22 @@
 {
   description = "dev shell that uses the user's configured shell";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            shellHook = ''
-              # Get the user's default shell from /etc/passwd
-              user_shell="$(awk -F: -v user="$(whoami)" '$1 == user {print $NF}' /etc/passwd)"
+  inputs = {};
+  outputs = { self, ... }: {
+    lib.mkUserShell = { pkgs, ... }@args:
+      pkgs.mkShell ({
+        shellHook = ''
+          # Get the user's default shell from /etc/passwd
+          user_shell="$(awk -F: -v user="$(whoami)" '$1 == user {print $NF}' /etc/passwd)"
 
-              # Check if the shell is interactive
-              if [[ $- == *i* ]]; then
-                echo "Starting user's default shell: $user_shell"
-                exec "$user_shell"
-              else
-                echo "Non-interactive shell detected. Using default nix-shell."
-              fi
-            '';
-          };
-        }
-      );
+          # Check if the shell is interactive
+          if [[ $- == *i* ]]; then
+            echo "Starting user's default shell: $user_shell"
+            exec "$user_shell"
+          else
+            echo "Non-interactive shell detected. Using default nix-shell."
+          fi
+        '';
+      } // builtins.removeAttrs args [ "pkgs" ] );
+  };
 }
 
